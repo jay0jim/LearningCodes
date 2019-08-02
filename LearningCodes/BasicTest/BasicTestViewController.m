a3c39d9cbb171dbeb3f4f054829f4497a8682722
jay0jim <284096905@qq.com>
2019-08-03 00:47:45 +0800

    测试深浅复制

--- LearningCodes/BasicTest/BasicTestViewController.m
+++ LearningCodes/BasicTest/BasicTestViewController.m
@@ -43,17 +43,106 @@
 //
 //    NSLog(@"%@", self.dicForMutableTest);
     
-    NSMutableDictionary *aDic = [NSMutableDictionary dictionaryWithDictionary:@{@"1": @"1"}];
+//    NSMutableDictionary *aDic = [NSMutableDictionary dictionaryWithDictionary:@{@"1": @"1"}];
+//
+//    NSDictionary *bDic = [aDic copy];
+//
+//    NSDictionary *cDic = [aDic mutableCopy];
+//
+//    [aDic setObject:@"2" forKey:@"2"];
+//
+//    JMPLog(aDic[@"1"]);
+//    JMPLog(bDic[@"1"]);
+//    JMPLog(cDic[@"1"]);
     
-    NSDictionary *bDic = [aDic copy];
+    // 基本数据类型
+//    int a = 0;
+//    int b = a;
+//
+//    JMPLog(&a);
+//    JMPLog(&b);
     
-    NSDictionary *cDic = [aDic mutableCopy];
+//    // 不可变非集合类型对象
+//    NSString *strA = @"strA";
+//    NSString *strB = [strA copy];
+//    NSString *strC = [strA mutableCopy];
+//    NSLog(@"Value -- strA: %@, strB: %@, strC: %@", strA, strB, strC);
+//    NSLog(@"Pointer -- strA: %p, strB: %p, strC: %p", &strA, &strB, &strC);
+//    NSLog(@"Pointer of value -- strA: %p, strB: %p, strC: %p", strA, strB, strC);
     
-    [aDic setObject:@"2" forKey:@"2"];
+//    // 可变非集合类型对象
+//    NSMutableString *strA = [NSMutableString stringWithFormat:@"strA"];
+//    NSString *copyStr = [strA copy];
+//    NSString *copyStr2 = [strA copy];
+//    NSMutableString *copyMStr = [strA copy];
+//    NSMutableString *copyMStr2 = [strA copy];
+//    NSString *mutableCopyStr = [strA mutableCopy];
+//    NSString *mutableCopyStr2 = [strA mutableCopy];
+//    NSMutableString *mutableCopyMStr = [strA mutableCopy];
+//    NSMutableString *mutableCopyMStr2 = [strA mutableCopy];
+//
+//    NSLog(@"strA -- %p &strA -- %p", strA, &strA);
+//    NSLog(@"copyStr -- %p &copyStr -- %p", copyStr, &copyStr);
+//    NSLog(@"copyStr2 -- %p &copyStr2 -- %p", copyStr2, &copyStr2);
+//    NSLog(@"copyMStr -- %p &copyMStr -- %p", copyMStr, &copyMStr);
+//    NSLog(@"copyMStr2 -- %p &copyMStr2 -- %p", copyMStr2, &copyMStr2);
+//    NSLog(@"mutableCopyStr -- %p &mutableCopyStr -- %p", mutableCopyStr, &mutableCopyStr);
+//    NSLog(@"mutableCopyStr2 -- %p &mutableCopyStr2 -- %p", mutableCopyStr2, &mutableCopyStr2);
+//    NSLog(@"mutableCopyMStr -- %p &mutableCopyMStr -- %p", mutableCopyMStr, &mutableCopyMStr);
+//    NSLog(@"mutableCopyMStr2 -- %p &mutableCopyMStr2 -- %p", mutableCopyMStr2, &mutableCopyMStr2);
     
-    JMPLog(aDic[@"1"]);
-    JMPLog(bDic[@"1"]);
-    JMPLog(cDic[@"1"]);
+    // 不可变集合类型对象
+//    NSDictionary *aDic = @{@"aaa": @"111"};
+//
+//    NSDictionary *copyDic = [aDic copy];
+//    NSDictionary *copyDic2 = [aDic copy];
+//    NSMutableDictionary *copyMDic = [aDic copy];
+//    NSMutableDictionary *copyMDic2 = [aDic copy];
+//
+//    NSDictionary *mutableCopyDic = [aDic mutableCopy];
+//    NSDictionary *mutableCopyDic2 = [aDic mutableCopy];
+//    NSMutableDictionary *mutableCopyMDic = [aDic mutableCopy];
+//    NSMutableDictionary *mutableCopyMDic2 = [aDic mutableCopy];
+//
+//    NSLog(@"aDic -- %p aDic[@\"aaa\"] -- %p key aaa -- %p", aDic, aDic[@"aaa"], [[aDic allKeys] firstObject]);
+//
+//    NSLog(@"copyDic -- %p copyDic[@\"aaa\"] -- %p key aaa -- %p", copyDic, copyDic[@"aaa"], [[copyDic allKeys] firstObject]);
+//    NSLog(@"copyDic2 -- %p copyDic2[@\"aaa\"] -- %p key aaa -- %p", copyDic2, copyDic2[@"aaa"], [[copyDic2 allKeys] firstObject]);
+//    NSLog(@"copyMDic -- %p copyMDic[@\"aaa\"] -- %p key aaa -- %p", copyMDic, copyMDic[@"aaa"], [[copyMDic allKeys] firstObject]);
+//    NSLog(@"copyMDic2 -- %p copyMDic2[@\"aaa\"] -- %p key aaa -- %p", copyMDic2, copyMDic2[@"aaa"], [[copyMDic2 allKeys] firstObject]);
+//
+//    NSLog(@"mutableCopyDic -- %p mutableCopyDic[@\"aaa\"] -- %p key aaa -- %p", mutableCopyDic, mutableCopyDic[@"aaa"], [[mutableCopyDic allKeys] firstObject]);
+//    NSLog(@"mutableCopyDic2 -- %p mutableCopyDic2[@\"aaa\"] -- %p key aaa -- %p", mutableCopyDic2, mutableCopyDic2[@"aaa"], [[mutableCopyDic2 allKeys] firstObject]);
+//    NSLog(@"mutableCopyMDic -- %p mutableCopyMDic[@\"aaa\"] -- %p key aaa -- %p", mutableCopyMDic, mutableCopyMDic[@"aaa"], [[mutableCopyMDic allKeys] firstObject]);
+//    NSLog(@"mutableCopyMDic2 -- %p mutableCopyMDic2[@\"aaa\"] -- %p key aaa -- %p", mutableCopyMDic2, mutableCopyMDic2[@"aaa"], [[mutableCopyMDic2 allKeys] firstObject]);
+    
+    // 可变集合类型对象
+    NSMutableDictionary *aDic = [NSMutableDictionary dictionaryWithCapacity:10];
+    [aDic setObject:@"111" forKey:@"aaa"];
+    
+    NSDictionary *copyDic = [aDic copy];
+    NSDictionary *copyDic2 = [aDic copy];
+    NSMutableDictionary *copyMDic = [aDic copy];
+    NSMutableDictionary *copyMDic2 = [aDic copy];
+    
+    NSDictionary *mutableCopyDic = [aDic mutableCopy];
+    NSDictionary *mutableCopyDic2 = [aDic mutableCopy];
+    NSMutableDictionary *mutableCopyMDic = [aDic mutableCopy];
+    NSMutableDictionary *mutableCopyMDic2 = [aDic mutableCopy];
+    
+    NSLog(@"aDic -- %p aDic[@\"aaa\"] -- %p key aaa -- %p", aDic, aDic[@"aaa"], [[aDic allKeys] firstObject]);
+    
+    NSLog(@"copyDic -- %p copyDic[@\"aaa\"] -- %p key aaa -- %p", copyDic, copyDic[@"aaa"], [[copyDic allKeys] firstObject]);
+    NSLog(@"copyDic2 -- %p copyDic2[@\"aaa\"] -- %p key aaa -- %p", copyDic2, copyDic2[@"aaa"], [[copyDic2 allKeys] firstObject]);
+    NSLog(@"copyMDic -- %p copyMDic[@\"aaa\"] -- %p key aaa -- %p", copyMDic, copyMDic[@"aaa"], [[copyMDic allKeys] firstObject]);
+    NSLog(@"copyMDic2 -- %p copyMDic2[@\"aaa\"] -- %p key aaa -- %p", copyMDic2, copyMDic2[@"aaa"], [[copyMDic2 allKeys] firstObject]);
+    
+    NSLog(@"mutableCopyDic -- %p mutableCopyDic[@\"aaa\"] -- %p key aaa -- %p", mutableCopyDic, mutableCopyDic[@"aaa"], [[mutableCopyDic allKeys] firstObject]);
+    NSLog(@"mutableCopyDic2 -- %p mutableCopyDic2[@\"aaa\"] -- %p key aaa -- %p", mutableCopyDic2, mutableCopyDic2[@"aaa"], [[mutableCopyDic2 allKeys] firstObject]);
+    NSLog(@"mutableCopyMDic -- %p mutableCopyMDic[@\"aaa\"] -- %p key aaa -- %p", mutableCopyMDic, mutableCopyMDic[@"aaa"], [[mutableCopyMDic allKeys] firstObject]);
+    NSLog(@"mutableCopyMDic2 -- %p mutableCopyMDic2[@\"aaa\"] -- %p key aaa -- %p", mutableCopyMDic2, mutableCopyMDic2[@"aaa"], [[mutableCopyMDic2 allKeys] firstObject]);
+    
+
     
 }
 
